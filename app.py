c1857b1847e4a3effc0365c0b0c94f0e7d74ccf0
amalshaji <amalshajid@gmail.com>
2020-06-20 15:26:08 +0530

    tracking added

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ f = open('sentiment_model.pickle', 'rb')
 classifier = pickle.load(f)
 f.close()
 
-st.title('Twitter Live Sentiment Visualizer')
+st.title('Twitter Live Sentiment Visualizer (beta)')
 
 st.sidebar.title('Enter a hashtag')
 hashtag = st.sidebar.text_input('hashtag', 'trump')
-date = st.sidebar.date_input('Analyse tweets from,', datetime.date(2020, 6, 18))
+date = st.sidebar.date_input('Analyse tweets from', datetime.date(2020, 6, 18))
 
 if st.sidebar.button('Live analysis', key='analyse'):
     pos_count = 0
@@ -42,6 +42,9 @@ if st.sidebar.button('Live analysis', key='analyse'):
         lang='en',
         since=date
     ).items()
+    total_tweets = st.empty()
+    pos_tweets = st.empty()
+    neg_tweets = st.empty()
     chart = st.line_chart(df)
     # chart2 = st.bar_chart(df.tail(1))
     for idx, tweet in enumerate(tweets):
@@ -51,6 +54,9 @@ if st.sidebar.button('Live analysis', key='analyse'):
             neg_count += 1
         elif output == "Positive":
             pos_count += 1
+        total_tweets.text('Tweets Analysed: %d' % tweets_count)
+        pos_tweets.text('Positive tweets: %d' % pos_count)
+        neg_tweets.text('Negative tweets: %d' % neg_count)
         # st.write(neg_count, pos_count)
         df2 = pd.DataFrame({"Positive": [pos_count], "Negative": [neg_count]})
         chart.add_rows(df2)
