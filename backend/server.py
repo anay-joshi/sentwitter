0db1a550494432ca3cc00d55a025fa0dfa8ebc4b
Amal Shaji <amalshaji@tutanota.com>
2020-08-13 23:50:40 +0530

    changed server to start programatically

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 import nltk
+import uvicorn
 import utils as utils
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -27,3 +28,8 @@ def analyse_tweet(tweet: Tweet):
     custom_tokens = remove_noise(word_tokenize(tweet.tweet))
     result = classifier.classify(dict([token, True] for token in custom_tokens))
     return {"sentiment": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", log_level="info")
+
